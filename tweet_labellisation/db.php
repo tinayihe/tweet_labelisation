3773afe9d4e4145ff8acdd834d4c9ba88e63aac4
Yihe <tinayihe39@hotmail.com>
2017-03-24 13:12:31 +0100

    vider le branch

--- tweet_labellisation/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function dbConnect() {
-	$dsn = "mysql:host=127.0.0.1;port=3306;dbname=tweet_labelisation";
-	$username = "root";
-	$password = "root";
-	$db = new PDO($dsn, $username, $password);
-	return $db;
-}
